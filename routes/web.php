be0e97c7846c6bfe5fc662869b75dec267a7b882
DJeremy <96179359+Jeloumy@users.noreply.github.com>
2023-10-18 11:41:59 +0200

    Création Oauth google et github

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,8 @@
 <?php
 
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\SocialiteController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -16,3 +17,32 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::prefix('/blog')->name('blog.')->group(function (){
+Route::get('/', function (Request $request) {
+
+    return [
+        "link" => route('blog.show', ['slug' => 'article', 'id' => 13]),
+    ];
+})->name('index');
+
+Route::get('/{slug}-{id}', function(string $slug, string $id, Request $request){
+    return [
+        "slug" => $slug,
+        'id' => $id,
+        'name'=> $request->input('name'),
+    ];
+})->where([
+    'id'=>'[0-9]+',
+    'slug'=>'[a-z0-9\-]+'
+])->name('show');
+});
+
+# Socialite URLs
+
+
+// La redirection vers le provider
+Route::get("redirect/{provider}", [SocialiteController::class,'redirect'])->name('socialite.redirect');
+
+// Le callback du provider
+Route::get("callback/{provider}", [SocialiteController::class,'callback'])->name('socialite.redirect');
